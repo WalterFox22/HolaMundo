ae34660d0749a9b89a57d58363d435d329b16125
1-21-01-E043-27\Esf0t <richardpadilla3rod@gmail.com>
2024-05-23 09:12:57 -0500

    hice Do desde 6 hasta 13

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Hello World");
+            System.out.println("MENU");
+            System.out.println("1. Videojuegos");
+            System.out.println("2. Videojuegos");
+            System.out.println("3. Videojuegos");
+            System.out.println("4. Videojuegos");
+        } while (sc.hasNextInt());
     }
-    //Hola mundodddss
-            // como estas
+    //Hola mundo
 }
 
-// CONFIRMA ESE DEBER DE POO
-// COMO ESTAS
-
-//YINIBOCBA
-
-// COME ON
